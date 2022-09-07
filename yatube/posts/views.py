f0c0434fb2cbacdcd3f09b1b195a491251e39169
Зубков Алексей <psi.alexeyzubkov@yandex.ru>
2022-09-07 19:55:29 +0300

    vol2.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,13 @@
-from multiprocessing import context
-from turtle import title
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
-import sys
+
 
 def index(request):
     # Формируем шаблон
     template = 'posts/index.html'
     post_list = Post.objects.select_related('group').order_by('-pub_date')[:10]
     context = {
-        'post_list' : post_list
+        'post_list':post_list
     }
     return render(request, template, context)
 
@@ -20,10 +16,10 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group = group).order_by('-pub_date')[:10]
     context = {
-        'group': group,
-        'posts': posts
+        'group':group,
+        'posts':posts
     }
-    return render(request, template, context) 
+    return render(request, template, context)
 
 
 
