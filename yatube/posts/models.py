3bf1e2b7b47d49327fbb2d997c479d290537b79e
Зубков Алексей <psi.alexeyzubkov@yandex.ru>
2022-09-07 20:41:45 +0300

    vol2.7

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -22,6 +22,6 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('дата публикации', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                            related_name='posts')
+                               related_name='posts')
     group = models.ForeignKey(Group, on_delete=models.CASCADE,
-                            blank=True, null=True)
+                              blank=True, null=True)
