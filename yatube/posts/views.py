d82f66d975fd9ffc08308e1a9b24ac4e0efbfc8b
Зубков Алексей <psi.alexeyzubkov@yandex.ru>
2022-09-07 20:24:07 +0300

    vol2.4

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,16 +7,18 @@ def index(request):
     template = 'posts/index.html'
     post_list = Post.objects.select_related('group').order_by('-pub_date')[:10]
     context = {
-        'post_list' : post_list
+        'post_list': post_list
     }
     return render(request, template, context)
+
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group = group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'group' : group,
-        'posts' : posts
-    }
+        'group': group,
+        'posts': posts
+        }
     return render(request, template, context)
+    
