421cdcb727efc47fb1ae36ababc32662c34c804a
Зубков Алексей <psi.alexeyzubkov@yandex.ru>
2022-09-10 19:57:15 +0300

    10.09.22_upd_vol2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,11 +4,13 @@ from .models import Post, Group
 
 text_output: str = 10
 
+
 def index(request):
     template = 'posts/index.html'
-    post_list = Post.objects.select_related('group').order_by('-pub_date')[:text_output]
+    post_list = Post.objects.select_related
+    ('group').order_by('-pub_date')[:text_output]
     context = {
-        'post_list' : post_list
+        'post_list': post_list
     }
     return render(request, template, context)
 
@@ -16,7 +18,8 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group = group).order_by('-pub_date')[:text_output]
+    posts = Post.objects.filter(group = group).order_by
+    ('-pub_date')[:text_output]
     context = {
         'group': group,
         'posts': posts
